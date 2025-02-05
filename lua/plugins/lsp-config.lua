a1782f302bb2f9f3890b0746bd24246bd866ef2d
shaneobby <163554908+shaneobby@users.noreply.github.com>
2025-02-05 11:14:20 +0530

    random

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,38 +1,71 @@
 return {
-	{
-		"j-hui/fidget.nvim",
-		opts = {
-			-- options
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "jdtls", "pyright" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.jdtls.setup({})
-			lspconfig.pyright.setup({})
+  {
+    "j-hui/fidget.nvim",
+    opts = {
+      -- options
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      -- import mason
+      local mason = require("mason")
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "<C-w>d", vim.diagnostic.open_float, {})
-		end,
-	},
+      -- import mason-lspconfig
+      local mason_lspconfig = require("mason-lspconfig")
+
+      local mason_tool_installer = require("mason-tool-installer")
+
+      -- enable mason and configure icons
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+
+      mason_lspconfig.setup({
+        -- list of servers for mason to install
+        ensure_installed = {
+          "lua_ls",
+          "ast_grep",
+          "jdtls",
+        },
+      })
+
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "stylua", -- lua formatter
+          "pylint",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.jdtls.setup({
+        cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
+        root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
+      })
+      lspconfig.ast_grep.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "<C-w>d", vim.diagnostic.open_float, {})
+      vim.keymap.set("i","jj","<esc>",{})
+    end,
+  },
 }
