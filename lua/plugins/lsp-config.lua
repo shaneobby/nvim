b49defeb92c64e318047e7d51768f5e67a32ea0f
shaneobby <163554908+shaneobby@users.noreply.github.com>
2025-10-11 18:53:54 +0530

    .

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,71 +1,62 @@
 return {
+  -- Fidget: LSP progress/status UI
   {
     "j-hui/fidget.nvim",
     opts = {
-      -- options
+      progress = {
+        display = {
+          render_limit = 10, -- show up to 10 simultaneous tasks
+        },
+      },
+      notification = {
+        window = {
+          normal_hl = "Normal",
+          winblend = 0,
+        },
+      },
     },
   },
+
+  -- Mason: LSP/DAP/Linter/Formatter manager
   {
     "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    build = ":MasonUpdate",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     },
-    config = function()
-      -- import mason
-      local mason = require("mason")
-
-      -- import mason-lspconfig
-      local mason_lspconfig = require("mason-lspconfig")
-
-      local mason_tool_installer = require("mason-tool-installer")
-
-      -- enable mason and configure icons
-      mason.setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-
-      mason_lspconfig.setup({
-        -- list of servers for mason to install
-        ensure_installed = {
-          "lua_ls",
-          "ast_grep",
-          "jdtls",
-        },
-      })
-
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "stylua", -- lua formatter
-          "pylint",
-        },
-      })
-    end,
   },
+
+  -- Mason-LSPConfig: Bridge between Mason and nvim-lspconfig
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.jdtls.setup({
-        cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-        root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
-      })
-      lspconfig.ast_grep.setup({})
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-      vim.keymap.set("n", "<C-w>d", vim.diagnostic.open_float, {})
-      vim.keymap.set("i","jj","<esc>",{})
-    end,
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "jdtls",
+      },
+      automatic_installation = true,
+    },
   },
+
+  -- Mason-Tool-Installer: For non-LSP tools (linters, formatters, etc.)
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "stylua",   -- Lua formatter
+        "prettier", -- JS/TS/HTML/CSS formatter
+        "eslint_d", -- JS/TS linter
+      },
+      auto_update = true,
+      run_on_start = true,
+    },
+  }, 
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -20,5 +20,12 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.schedule(function()
-  vim.opt.clipboard = "unnamedplus"
+  vim.opt.clipboard = "unnamedplus",
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+  vim.keymap.set("n", "<C-w>d", vim.diagnostic.open_float, {})
+  vim.keymap.set("i","jj","<esc>",{})
 end)
